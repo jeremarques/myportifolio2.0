88843d5c03ac7eb295a8e47a34d89d12175489a8
Jeremias Marques <saimerejmarques@gmail.com>
2024-10-08 00:22:26 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'black': '#050505',
+                'grass-green': '#1FC24D',
+                'sky-blue': '#3793FF',
+                'white': '#FDFDFD',
+                'light-gray': '#BBBBBB',
+                'gray': '#E8E8E8',
+                'gray2': '#B0B0B0',
+                'dark-gray': '#3D3D3D',
+            }
+        }
+    }
+}
